0e7538226e4237152bf7f50699b70375427f997f
emmikoivisto <emetsi@gmail.com>
2020-09-18 17:59:23 +0100

    gues attributes tested

--- src/songs.py
+++ src/songs.py
@@ -1,4 +1,10 @@
 class Songs:
+    
     def __init__(self, title, artist):
         self.title = title
         self.artist = artist
+        self.playlist = []
+
+    
+    def add_songs_to_playlist(self, new_song):
+        self.playlist.append(new_song)
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,5 +3,14 @@ from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
 
-    def setUp(self, name, age):
-        self.customer = ("Jack", 25)
+    def setUp(self):
+        self.guest = Guest("Jack", 25)
+    
+    def test_guest_has_name(self):
+        self.assertEqual("Jack", self.guest.name)
+
+    def test_guest_has_age(self):
+        self.assertEqual(25, self.guest.age)
+
+    def test_guest_has_wallet(self):
+        self.assertEqual([], self.guest.money)
