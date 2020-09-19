841fc53d07cb0e2fb3d136b62d5fef790b030c0d
emmikoivisto <emetsi@gmail.com>
2020-09-19 10:42:35 +0100

    guest count function added and tested

--- src/room.py
+++ src/room.py
@@ -1,5 +1,5 @@
 class Room:
-    def __init__(self, name):
+    def __init__(self, name, guest_list):
         self.name = name
         self.guest_list = guest_list
         self.playlist = []
@@ -8,5 +8,5 @@ class Room:
     def add_songs_to_playlist(self, new_song):
         self.playlist.append(new_song)
 
-    def guest_list(self):
+    def guest_count(self):
         return len(self.guest_list)
